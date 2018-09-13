05e7b6d8fa83c4578e079140361a2e9ad4927cd0
Priya <pvaidya56@gmail.com>
2018-09-13 20:44:11 +0000

    fixed errors

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
+/*This program prints a line of text*/
 #include <stdio.h>
 
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+  return (0);
 }

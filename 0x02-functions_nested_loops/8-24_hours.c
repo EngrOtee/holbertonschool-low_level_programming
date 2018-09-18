0db642372ef1a1cd1f092a08c4d1852b18a81230
Priya <pvaidya56@gmail.com>
2018-09-18 19:54:45 +0000

    initial commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,4 +11,16 @@ void jack_bauer(void)
 	int hr;
 	int min;
 
-	for 
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hr/10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min/10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}

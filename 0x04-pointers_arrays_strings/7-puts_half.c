3c96fa267b92d08376b151028e7267ecde6931c9
Priya Vaidya <pvaidya56@gmail.com>
2018-09-26 03:03:35 +0000

    fixed spacing

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -17,8 +17,7 @@ void puts_half(char *str)
 		for (even = ct / 2; str[even] != '\0'; even++)
 			_putchar(str[even]);
 	}
-
-        else
+	else
 	{
 		for (neg = (ct - 1) / 2; str[neg] != '\0'; neg++)
 			_putchar(str[neg]);

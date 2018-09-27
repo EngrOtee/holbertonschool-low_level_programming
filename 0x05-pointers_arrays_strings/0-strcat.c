d1c1efdef4a4fcfcc11077e6c1d17831da992d9e
Priya Vaidya <pvaidya56@gmail.com>
2018-09-27 21:45:51 +0000

    initial commit

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -4,21 +4,23 @@
  * *_strcat - check the code for Holberton School students.
  *@dest: destination
  *@src: source
- * Return: Always 0.
+ * Return: pointer to the destination
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; src[i] != '\0'; i++)
-
-		for (j = 0; src[j] != '\0'; j++)
-		{
-			src[i] = dest[j];
-			i++;
-		}
-	dest[i] = src[j];
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+		j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	return (dest);
-
-
 }

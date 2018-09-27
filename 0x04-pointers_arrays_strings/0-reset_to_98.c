a9ae013342a431735e579eca54a86835797ff451
Priya Vaidya <pvaidya56@gmail.com>
2018-09-27 15:27:59 +0000

    initial commit

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
  * reset_to_98 - takes a pointer to an int and updates the value
  *@n: number being updated
  *

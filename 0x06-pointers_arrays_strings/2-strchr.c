756e539c97e2664fbf39e0783033f07f803393a1
Priya Vaidya <pvaidya56@gmail.com>
2018-10-02 02:36:17 +0000

    initial commit

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: a pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *null = NULL;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	if (s[i] == '\0')
+		return (s + i);
+	else
+		return (null);
+}

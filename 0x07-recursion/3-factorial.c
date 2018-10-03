701dd61bec5c573d21739dbc3b748c27bf38a09c
Priya Vaidya <pvaidya56@gmail.com>
2018-10-03 23:47:30 +0000

    initial commit

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		n = n * factorial(n - 1);
+	return (n);
+	}
+}

933f49fe33ed7cccf4844a3f97a0684addcda868
Priya Vaidya <pvaidya56@gmail.com>
2018-10-07 03:09:44 +0000

    Initial Commit

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * digit - Checks if the number is a digit
+ *@s: character being tested
+ *
+ * Return: Always 0.
+ */
+
+
+
+
+
+
+
+
+
+/**
+ * main - Program that multiplies two numbers
+ *@argc: count of arguments
+ *@argv: array of pointers to the strings passed
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{

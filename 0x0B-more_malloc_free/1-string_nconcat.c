262bbf34599212d2d02db933d04b87b07aa9ee92
Priya Vaidya <pvaidya56@gmail.com>
2018-10-11 23:48:06 +0000

    Initial commit

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < str1; i++)
 	{
-		a[i] =  s1[i];
+		a[i] = s1[i];
 	}
 	for (j = 0; j < str2; j++)
 		a[i + j] = s2[j];

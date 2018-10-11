89692c3393cef90ecf1bd830d550e43d56cc55fd
Priya Vaidya <pvaidya56@gmail.com>
2018-10-11 20:10:51 +0000

    initial commit

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -3,9 +3,8 @@
 #include <stdlib.h>
 
 /**
- * print_grid - prints a grid of integers
+ * free_grid - frees a 2D grid
  * @grid: the address of the two dimensional grid
- * @width: width of the grid
  * @height: height of the grid
  *
  * Return: Nothing.

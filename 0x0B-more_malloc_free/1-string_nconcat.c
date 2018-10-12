304a79c7f7c34fce076d271b1e50c6afa6ca406b
Priya Vaidya <pvaidya56@gmail.com>
2018-10-12 00:03:00 +0000

    fixed loop condition

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -16,13 +16,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, str1, str2;
 
 	if (s1 == NULL)
-		return (" ");
+		return ("");
 	if (s2 == NULL)
-		return (" ");
+		return ("");
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; s2[j] != '\0' && j < n; j++)
+	for (j = 0; s2[j] != '\0' && j <= n; j++)
 		;
 	str1 = i;
 	str2 = j;

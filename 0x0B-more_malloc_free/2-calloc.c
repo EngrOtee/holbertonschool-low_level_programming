999a7aad25398e12f6c4961164fc3932ec888ed4
Priya Vaidya <pvaidya56@gmail.com>
2018-10-12 00:40:17 +0000

    Intial Commit

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc( nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		a[i] = 0;
+	return (a);
+}

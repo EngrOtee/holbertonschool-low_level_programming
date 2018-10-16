b766c4f563775ee1fb9fb729ccb5c2ca394dfcd9
Priya Vaidya <pvaidya56@gmail.com>
2018-10-16 20:23:24 +0000

    Added semicolon

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,7 @@
 #include "dog.h"
 #include <stdlib.h>
 char *_strdup(char *str);
+int str_len(char *s);
 /**
  * new_dog - creates a new dog object
  *@name: name of dog
@@ -58,4 +59,23 @@ char *_strdup(char *str)
 	for (i = 0; i < str_len; i++)
 		dupe[i] = str[i];
 	return (dupe);
+}
+
+/**
+ * _strlen - returns the length of the string
+ *@s: string being counted
+ * Return: Always 0.
+ */
+
+int str_len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }

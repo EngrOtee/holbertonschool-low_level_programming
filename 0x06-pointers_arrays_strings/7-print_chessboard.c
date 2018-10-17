624767291e491c0c2eefb564e0aaacf7a3f6d7c8
Priya Vaidya <pvaidya56@gmail.com>
2018-10-17 03:01:35 +0000

    Initial Commit

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_chessboard - prints the chessboard
+ *@a: the array printed
  * Return: Always 0.
  */
 
 void print_chessboard(char (*a)[8])
 {
+	int x, b, c;
+
+	c = 0;
+	while (a[c][c])
+		c++;
+	c -= 1;
+	for (x = 0; x < c; x++)
+	{
+		for (b = 0; b < c; b++)
+			_putchar(a[x][b]);
+		_putchar('\n');
+	}
+}

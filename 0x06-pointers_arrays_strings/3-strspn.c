2799f88f967b3efa393176dc4532a195a6e9cb4d
Priya Vaidya <pvaidya56@gmail.com>
2018-10-17 02:41:08 +0000

    Initial Commit

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -2,23 +2,30 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _strspn - Gets the length of a prefix substring
+ *@s: String that is tested
+ *@accept: bytes checked
+ * Return: Number of bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	int i = 0, j, a = 1;
+	unsigned int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0' && a == 1)
 	{
-		exit = 0;
-		for(j = 0; accept[j] != '\0'; j++)
+		a = 0;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if ( accept[j] == 0)
-				return (s + i);
+			if (s[i] == accept[j])
+			{
+				count++;
+				a = 1;
+				break;
+			}
 		}
+		i++;
 	}
-	return (0);
+	return (count);
 }

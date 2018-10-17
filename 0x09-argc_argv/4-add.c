7cba4e1cbcd20cc0e46127b4120ffedba98b4f5e
Priya Vaidya <pvaidya56@gmail.com>
2018-10-17 02:09:43 +0000

    Changed for loop condition

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -2,27 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * digit - Checks if the number is a digit
- *@s: character being tested
- *
- * Return: Always 0.
- */
-
-
-
-
-
-
-
-
 
 /**
- * main - Program that multiplies two numbers
+ * main - Program that adds two numbers
  *@argc: count of arguments
  *@argv: array of pointers to the strings passed
- * Return: Always 0.
+ * Return: Always 0, return 1 if an error
  */
 
 int main(int argc, char *argv[])
 {
+	int sum = 0, a, b, x;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		x = atoi(argv[a]);
+		sum += x;
+	}
+	printf("%d\n", sum);
+	return (0);
+}

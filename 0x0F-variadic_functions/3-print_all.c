4e47f606c7f635b293254c4ff5478b192ffa3cd5
Priya Vaidya <pvaidya56@gmail.com>
2018-10-20 01:01:59 +0000

    Initial Commit

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints anything
+ *@
+ *@
+ * Return: The passed argument
+ */
+
+void print_all(const char * const format, ...)
+{

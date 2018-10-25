1472c4322dd265cc78e12ff01eb637953ecbf5db
Priya Vaidya <pvaidya56@gmail.com>
2018-10-25 22:56:54 +0000

    Fixed betty errors

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -13,19 +13,34 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	int len = 0;
 
 	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	for (len = 0; str[len]; len++)
-		;
 
 	new_node->str = strdup(str);
-	new_node->len = len;
+	new_node->len = _strlen(str);
 
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
+}
+
+/**
+ * _strlen - returns the length of the string
+ *@s: string being counted
+ * Return: Always 0.
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }

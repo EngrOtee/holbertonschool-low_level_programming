ab5e6ceaa3d3d2d8f1bf12b34bf82e42b54755e5
Priya <pvaidya56@gmail.com>
2018-11-07 23:47:53 +0000

    Initial Commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX standard output
+ *@filename: Pointer to file
+ *@letters: Number of letters it could read and print
+ * Return: Number of letters it could read and print or 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int writebuff, readbuff, fd;
+	char *buffptr;
+
+	buffptr = malloc(sizeof(char) * (letters + 1));
+	if (buffptr == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	{
+		close(fd);
+		free(buffptr);
+		return (0);
+	}
+	readbuff =  read(fd, buffptr, letters);
+	if (readbuff < 0)
+	{
+		close(fd);
+		free(buffptr);
+		return (0);
+	}
+	buffptr[letters] = '\0';
+	writebuff = write(STDOUT_FILENO, buffptr, letters);
+	if (writebuff <= 0)
+	{
+		close(fd);
+		free(buffptr);
+		return (0);
+	}
+	close(fd);
+	free(buffptr);
+	return (writebuff);
+}
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
